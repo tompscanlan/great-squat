f6076afee541a6c80daf54105779dd38dd1484fa
Tom Scanlan <tompscanlan@gmail.com>
2023-08-30 14:24:09 -0400

    general updates
    * throttle queries
    * try generator pattern
    * try in a thread

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+#![warn(future_incompatible, rust_2018_compatibility, rust_2018_idioms, unused)]
+#![warn(clippy::pedantic)]
+#![cfg_attr(feature = "strict", deny(warnings))]
+
 use clap::Parser;
 
+use std::{thread, time, time::Duration};
+
 const DNS_SERVER: &str = "8.8.8.8:53";
+
+const THROTTLE_TIME: Duration = time::Duration::from_millis(200);
+
 /// Simple cli for testing dns lookups
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -17,44 +26,90 @@ struct Args {
 }
 
 fn main() {
+    // handle args
     let args: Args = Args::parse();
     let _ = args.dns_names.len() >= 1 || panic!("No DNS names provided");
     println!("{:?}", args);
 
-    let (mut lookup_tx, lookup_rx) = spmc::channel();
+    // create a channel that we'll send domains to look up
+    let (mut lookup_tx, lookup_rx) = spmc::channel::<String>();
 
+    // start child to do lookups
+    let join_handle = thread::spawn(move || {
+        // look up
+        while let Result::Ok(domain) = lookup_rx.recv() {
+            // child bails on keyword exit
+            if domain == "exit" {
+                break;
+            }
+            // todo: rate limit
+            thread::sleep(THROTTLE_TIME);
+
+            let resolved = domain_name_resolves(args.dns_server.to_owned(), domain.to_owned());
+
+            if resolved {
+                println!("{} resolves", domain)
+            }
+        }
+    });
+
+    // send domains to look up
     for domain in args.dns_names {
         for d in generate_squatter_possibilities(domain) {
             lookup_tx.send(d).unwrap();
         }
     }
 
-    while let Result::Ok(domain) = lookup_rx.recv() {
-        println!(
-            "Hello {}! resolves? {}",
-            domain,
-            domain_name_resolves(args.dns_server.to_owned(), domain.to_owned())
-        );
-    }
+    let res = join_handle.join();
+    println!("res: {:?}", res);
 }
-
-// Levenshtein distance
-// soundex?
 
 fn generate_squatter_possibilities(domain: String) -> Vec<String> {
     let mut p = Vec::new();
+
+    // add variations
     p.push(format!("a{}", domain));
+
+    p.push(swap(domain.clone(), "a".to_string(), "s".to_string()));
+    p.push(swap(domain.clone(), "o".to_string(), "0".to_string()));
+   
+
+    // Levenshtein distance
+    // soundex?
+    //
+
+    p.push("exit".to_owned());
     return p;
+}
+
+#[test]
+fn test_twistrs() {
+    // use twistrs::permutate::Domain;
+    // let domain = Domain::new("google.com").unwrap();
+    // let permutations = domain.all().unwrap();
+    // println!("{:?}", permutations);
 }
 
 #[test]
 fn test_generate_squatter_possibilities() {
     let p = generate_squatter_possibilities("google.com".to_owned());
+    println!("{:?}", p);
     assert!(p.len() > 0)
 }
 
+fn swap(domain: String, find: String, replace: String) -> String {
+    return domain.replace(&find, &replace);
+}
+
+#[test]
+
+fn test_swap() {
+    let s = swap("google.com".to_owned(), 'l'.to_string(), '1'.to_string());
+    assert_eq!(s, "goog1e.com");
+} // fn squat_options(domain: String) -{
+  // }
+
 fn domain_name_resolves(server: String, domain: String) -> bool {
-    // Do the look ups
     use std::str::FromStr;
     use trust_dns_client::client::{Client, SyncClient};
     use trust_dns_client::op::DnsResponse;
@@ -86,24 +141,22 @@ fn domain_name_resolves(server: String, domain: String) -> bool {
     } else {
         return false;
     }
-    // Records are generic objects which can contain any data.
-    //  In order to access it we need to first check what type of record it is
-    //  In this case we are interested in A, IPv4 address
-    //   if let Some(RData::A(ref ip)) = answers[0].data() {
-    //       assert_eq!(*ip, A::new(93, 184, 216, 34))
-    //   } else {
-    //       assert!(false, "unexpected result")
-    //   }
 }
 
 #[test]
 fn test_good_resolution() {
-    assert!(domain_name_resolves(DNS_SERVER.to_string(), "google.com".to_owned()))
+    assert!(domain_name_resolves(
+        DNS_SERVER.to_string(),
+        "google.com".to_owned()
+    ))
 }
 #[test]
 fn test_does_not_resolve() {
     assert!(
-        !domain_name_resolves(DNS_SERVER.to_string(), "this-name-cant-really-exist.com".to_owned()),
+        !domain_name_resolves(
+            DNS_SERVER.to_string(),
+            "this-name-cant-really-exist.com".to_owned()
+        ),
         "this domain should never resolve"
     );
 }
