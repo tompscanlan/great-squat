da0a1a20b52cd416620890c279f0a67db29e6722
Tom Scanlan <tompscanlan@gmail.com>
2023-09-01 17:53:23 -0400

    restructure

---
+++ frontend/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+let domain = ref("google.com")
+
+
+const checkForSquatters = () => {
+    console.log(domain.value)
+}
+</script>
+
+<template>
+    <div>
+        <h1>Great Squat</h1>
+
+        <input type="text" v-model="domain" />
+        <button @click="checkForSquatters">Check for Squatters</button>
+    </div>
+</template>
+  
